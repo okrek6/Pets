245cd8969b2c74d16e4561903020d5a3a760423f
Brendan Krekeler <36323683+okrek6@users.noreply.github.com>
2020-03-05 23:49:25 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
